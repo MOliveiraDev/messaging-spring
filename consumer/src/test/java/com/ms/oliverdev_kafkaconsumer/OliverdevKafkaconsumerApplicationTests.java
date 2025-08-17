3a46c488ab18e8cbdf3f086fe0c5ed6cede98182
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-08-16 21:10:11 -0300

    add profile test

--- consumer/src/test/java/com/ms/oliverdev_kafkaconsumer/OliverdevKafkaconsumerApplicationTests.java
+++ consumer/src/test/java/com/ms/oliverdev_kafkaconsumer/OliverdevKafkaconsumerApplicationTests.java
@@ -2,8 +2,10 @@ package com.ms.oliverdev_kafkaconsumer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class OliverdevKafkaconsumerApplicationTests {
 
 	@Test
--- producer/src/test/java/com/ms/oliverdev_kafkaproducer/OliverdevKafkaproducerApplicationTests.java
+++ producer/src/test/java/com/ms/oliverdev_kafkaproducer/OliverdevKafkaproducerApplicationTests.java
@@ -2,8 +2,10 @@ package com.ms.oliverdev_kafkaproducer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class OliverdevKafkaproducerApplicationTests {
 
 	@Test
